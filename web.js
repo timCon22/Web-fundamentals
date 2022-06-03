a41bc7a0717191d8773bddb2dfeaa71999803ce9
tim <tim@LAPTOP-UE1D4QC6.localdomain>
2022-06-02 20:51:28 -0600

    web.js

---
+++ web.js
@@ -0,0 +1,17 @@
+// 1. HTTP is Hypertext Transfer Protocol, is it how browsers and servers communicate
+
+// 2. A URL is a universal resourse locator, it is comprised of a protocol, hostname, port, resourse, and a query String
+
+// 3. A DNS is a hostname that points to the IP address of a website
+
+// 4. A query string mainly provides extra information such as search terms, info from forms, etc and has multiple arguements separated by &
+
+// 5. GET and POST, GET is mainly used for requesting a webpage whereas POST is used for changing or sending data to a webpage
+
+// 6. A method, http protocol, the resourse URL you want, and Headers
+
+// 7. An http protocol version, response status code, and Headers
+
+// 8. The hostname you're asking about, the date your browser thinks it is, language your browser wants information in, any cookies the server wants to set, and any caching information
+
+// 9. First the browser checks what protocol the url is, next it gets the hostname of the website, then it gets the port number for the site, and last it gets any query string that has been set for the page
